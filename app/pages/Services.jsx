7534fb32c7ff387e8140be73833a4bfe7bb6a0f7
Md Arif <arif.20527@sscbs.du.ac.in>
2024-10-08 15:56:13 +0530

    Union removed bggrid added to services

--- app/pages/Services.jsx
+++ app/pages/Services.jsx
@@ -14,12 +14,12 @@ const Services = () => {
         </p>
         {/* Cards */}
         <div className="flex justify-center mt-8 flex-wrap">
-          <div  className="relative w-[400px] h-[420px] mx-3 my-3  border-[1px] border-[#007443A3] rounded-lg bg-gradient-to-tr from-0% from-[#79f7b631] to-[#001C10] to-90% bg-opacity-30">
-          <div className="absolute h-[100%] w-[100%] bg-[url('/services/union.png')]">
-
-          </div>
+          <div className="relative w-[400px] h-[420px] mx-3 my-3  border-[1px] border-[#007443A3] rounded-lg bg-gradient-to-tr from-0% from-[#79f7b631] to-[#001C10] to-90% bg-opacity-30">
+            <div className="absolute h-[100%] w-[100%] bg-[url('/services/bggrid.png')]"></div>
             <div className="m-5 relative h-[90%] ">
-              <div className="w-[100px] h-[100px] "><Image src="/services/social.svg" height={100} width={100}/></div>
+              <div className="w-[100px] h-[100px] ">
+                <Image src="/services/social.svg" height={100} width={100} />
+              </div>
 
               <div className="text-start ">
                 <p className="text-lg sm:text-xl md:text-2xl font-bold mt-6 bg-clip-text text-transparent bg-gradient-to-br from-[#FFFFFF] from-40% to-[#4C685D] to-80%">
@@ -40,7 +40,9 @@ const Services = () => {
           </div>
           <div className="w-[400px] h-[420px] mx-3 my-3  border-[1px] border-[#007443A3] rounded-lg bg-gradient-to-tr from-0% from-[#79f7b631] to-[#001C10] to-9  0%">
             <div className="m-5 relative h-[90%] ">
-              <div className="w-[100px] h-[100px] "><Image src="/services/social.svg" height={100} width={100}/></div>
+              <div className="w-[100px] h-[100px] ">
+                <Image src="/services/social.svg" height={100} width={100} />
+              </div>
 
               <div className="text-start ">
                 <p className="text-lg sm:text-xl md:text-2xl font-bold mt-6 bg-clip-text text-transparent bg-gradient-to-br from-[#FFFFFF] from-40% to-[#4C685D] to-80%">
@@ -61,7 +63,9 @@ const Services = () => {
           </div>
           <div className="w-[400px] h-[420px] mx-3 my-3  border-[1px] border-[#007443A3] rounded-lg bg-gradient-to-tr from-0% from-[#79f7b631] to-[#001C10] to-9  0%">
             <div className="m-5 relative h-[90%] ">
-              <div className="w-[100px] h-[100px] "><Image src="/services/social.svg" height={100} width={100}/></div>
+              <div className="w-[100px] h-[100px] ">
+                <Image src="/services/social.svg" height={100} width={100} />
+              </div>
 
               <div className="text-start ">
                 <p className="text-lg sm:text-xl md:text-2xl font-bold mt-6 bg-clip-text text-transparent bg-gradient-to-br from-[#FFFFFF] from-40% to-[#4C685D] to-80%">
