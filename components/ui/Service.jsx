ae4332718b85a4d1677bb842d18c75fd57d85794
Md Arif <arif.20527@sscbs.du.ac.in>
2024-10-09 10:45:59 +0530

    Added key prop in service.jsx component

--- components/ui/Service.jsx
+++ components/ui/Service.jsx
@@ -5,7 +5,10 @@ const Service = ({ items }) => {
     <div className="flex justify-center mt-8 flex-wrap">
       {items.map((item, index) => {
         return (
-          <div className="relative w-[400px] h-[450px] mx-3 my-3  border-[1px] border-[#007443A3] rounded-lg bg-gradient-to-tr from-0% from-[#79f7b631] to-[#001C10] to-90% bg-opacity-30">
+          <div
+            key={item.image}
+            className="relative w-[400px] h-[450px] mx-3 my-3  border-[1px] border-[#007443A3] rounded-lg bg-gradient-to-tr from-0% from-[#79f7b631] to-[#001C10] to-90% bg-opacity-30"
+          >
             <div className="absolute h-[100%] w-[100%] bg-[url('/services/bggrid.png')]"></div>
             <div className="m-5 relative h-[90%] ">
               <div className="w-[100px] h-[100px] ">
